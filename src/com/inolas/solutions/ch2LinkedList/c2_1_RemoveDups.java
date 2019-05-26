0a14868862e16f943c265fa44f9a2f1b276ba2fb
Inolas <salonibuddhadeo@gmail.com>
2019-05-26 15:34:44 -0700

    JUnit test

---
+++ src/com/inolas/solutions/ch2LinkedList/c2_1_RemoveDups.java
@@ -0,0 +1,5 @@
+package com.inolas.solutions.ch2LinkedList;
+
+public class c2_1_RemoveDups
+{
+}
---
+++ src/test/solutions/MergeTwoSortedListsTest.java
@@ -0,0 +1,13 @@
+package test.solutions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeTwoSortedListsTest
+{
+    @Test
+    public void test1(){
+        assertEquals(1, 1);
+    }
+}
