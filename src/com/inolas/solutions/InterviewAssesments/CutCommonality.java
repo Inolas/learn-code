af1a23fbdf0b56cb3efa8a43502cfe92c68a33bc
Inolas <salonibuddhadeo@gmail.com>
2019-10-27 15:31:15 -0700

    Goldman Sachs | matrixgame

---
+++ src/com/inolas/solutions/InterviewAssesments/CutCommonality.java
@@ -0,0 +1,56 @@
+package com.inolas.solutions.InterviewAssesments;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CutCommonality
+{
+    public int max_cut_commonality(String str){
+
+        HashMap<Character, Integer> input_str = new HashMap<>();
+
+        for(char ch : str.toCharArray()) {
+            if(input_str.containsKey(ch))
+                input_str.replace(ch, input_str.get(ch)+1);
+            else
+                input_str.put(ch, 1);
+        }
+
+        int max_cut = 0;
+        int cut = 0;
+        for(int i=0; i<str.length(); ++i) {
+            char char_i = str.charAt(i);
+            if(input_str.get(char_i) > 1){
+                cut++;
+                input_str.replace(char_i, input_str.get(char_i) - 2);
+            } else if (input_str.get(char_i) == 0) {
+                cut--;
+            } else {
+                input_str.replace(char_i, input_str.get(char_i)-1);
+            }
+            max_cut = Math.max(cut, max_cut);
+        }
+        return max_cut;
+    }
+    public int chararray_cut(String str){
+        int[] count = new int[26];
+        for(char ch : str.toCharArray()) {
+            count[ch-'a']++;
+        }
+        int res = 0;
+        int cur = 0;
+        for(int i=0; i<str.length(); ++i) {
+            if(count[str.charAt(i)-'a'] > 1) {
+                ++cur;
+                count[str.charAt(i)-'a'] -= 2;
+            } else if (count[str.charAt(i)-'a'] == 0) {
+                --cur;
+            } else {
+                --count[str.charAt(i)-'a'];
+            }
+            res = Math.max(cur, res);
+        }
+        return res;
+    }
+}
---
+++ src/com/inolas/tests/InterviewAssesments/CutCommonalityTest.java
@@ -0,0 +1,36 @@
+package com.inolas.tests.InterviewAssesments;
+
+import com.inolas.solutions.InterviewAssesments.CutCommonality;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CutCommonalityTest
+{
+    CutCommonality mcc = new CutCommonality();
+
+    @Test
+    public void test1(){
+        int expected = 3;
+        int actual = mcc.max_cut_commonality("abcdedeara");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test2(){
+        int expected = 3;
+        int actual = mcc.max_cut_commonality("abcdecdefg");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test3(){
+        int expected = 4;
+        int actual = mcc.max_cut_commonality("zzzxxxzzz");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test_chararray(){
+        int expected = 3;
+        int actual = mcc.chararray_cut("abcdedeara");
+        assertEquals(expected, actual);
+    }
+}
