835fa22064f9939c117daf2eeba412680a99447d
Inolas <salonibuddhadeo@gmail.com>
2019-11-08 19:00:33 -0800

    deci-binary numbers

---
+++ src/com/inolas/tests/interview_assesments/DesiBinaryNumbersTest.java
@@ -0,0 +1,20 @@
+package com.inolas.tests.interview_assesments;
+
+import com.inolas.solutions.interview_assesments.DesiBinaryNumbers;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DesiBinaryNumbersTest
+{
+    DesiBinaryNumbers dbn = new DesiBinaryNumbers();
+    @Test
+    public void test1(){
+        dbn.solve(5);
+    }
+    @Test
+    public void test2(){
+        dbn.solve(21);
+    }
+
+}
