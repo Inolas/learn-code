787fe7da8897902df36006f8d23dbb166d505573
Inolas <salonibuddhadeo@gmail.com>
2020-01-01 15:01:17 -0800

    description to editDistance

--- src/com/inolas/solutions/algorithms/_editDistance.java
+++ src/com/inolas/solutions/algorithms/_editDistance.java
@@ -1,7 +1,10 @@
 package com.inolas.solutions.algorithms;
 
 /*
-* remove/delete - 1 (cart - cat); add - 1 (far - fart); change - 2 (cat - fat);
+* remove/delete = 1 (cart - cat); add = 1 (far - fart); change = 2 (cat - fat);
+* This approach uses memoization.
+* Storing the edit distance of each word in a 2D matrix fashion.
+* Complexity - O(n*m) :: n - length of first word; m - length of second word
 * */
 
 public class _editDistance
@@ -20,6 +23,7 @@ public class _editDistance
                 else if(j==0)
                     value = i;
                 else
+//                    minimum of top value + 1 (add); left value + 1 (delete) and diagonal values + 2 if the letter is not same(change).
                     value = minimum(distanceMatrix[i-1][j] + 1, distanceMatrix[i][j-1] + 1, distanceMatrix[i-1][j-1]+ (u.charAt(i-1) == v.charAt(j-1)? 0 : 2));
                 distanceMatrix[i][j] = value;
             }
@@ -29,12 +33,12 @@ public class _editDistance
     }
     public static int minimum(int a, int b, int c){
         int min;
-        min = c < b ? (c < a ? c : a) : (a < b ? a : b);
+            min = c < b ? (c < a ? c : a) : (a < b ? a : b);
         return min;
     }
     public static void main(String[] args)
     {
         System.out.println(_editDistance.editDistance("fast", "cats"));
-        System.out.println(_editDistance.editDistance("heat", "hit"));
+        System.out.println(_editDistance.editDistance("hot", "hit"));
     }
 }
